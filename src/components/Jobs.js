dc83bb7e26c7bad4784ec755657c7d244af77c48
Kwasi O-T <tweneboah21@gmail.com>
2023-09-06 23:29:11 +0000

    removed logo from the jobs component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 
-
 function Jobs({job}) {
 
     return (
         <div>
-            <img src={job.logo} alt='logo' />
             <p>{job.postedAt}</p>
             <p>{job.contract}</p>
             <p><b>{job.position}</b></p>
