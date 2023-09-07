e16f4e8b631ac69f2f6642efce056a5fa608591b
Kwasi O-T <tweneboah21@gmail.com>
2023-09-07 15:12:33 +0000

    added fontawesome script to index.html

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import Joblist from './components/Joblist';
 function App() {
   return (
     <div className="myApp">
-      <Joblist />
+      <div className='header'>
+        <div>
+          <h2>devjobs</h2>
+        </div>
+        <div></div>
+      </div>
+      <div className='joblist'>
+        <Joblist />
+      </div>
     </div>
   );
 }
