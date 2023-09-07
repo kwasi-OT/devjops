8a619ae9b5d597b48adebb4d0e1c4195912a42e1
Kwasi O-T <tweneboah21@gmail.com>
2023-09-07 17:05:32 +0000

    added mobile menu and styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import Joblist from './components/Joblist';
-
+// import fontawesome from './bars-solid.svg';
 
 
 function App() {
   return (
     <div className="myApp">
       <div className='header'>
-        <div>
-          <h2>devjobs</h2>
-        </div>
-        <div>
-        
+        <div className='headerContent'>
+          <div className='headingText'>
+            <h2>devjobs</h2>
+          </div>
+          <div className='mobileMenu'>
+            <i className="fa-solid fa-bars" style={{color: '#ffffff'}}></i>
+          </div>
         </div>
       </div>
       <div className='joblist'>
