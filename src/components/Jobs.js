7431615c4d96de213d809ac09c06d55dd45764ef
Kwasi O-T <tweneboah21@gmail.com>
2023-09-07 00:00:48 +0000

    added logo and background color to jobs

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -5,7 +5,7 @@ function Jobs({job}) {
 
     return (
         <div>
-            <img src={job.logo} alt='logo'/>
+            <img src={job.logo} alt='logo' style={{backgroundColor: job.logoBackground}}/>
             <p>{job.postedAt}</p>
             <p>{job.contract}</p>
             <p><b>{job.position}</b></p>
