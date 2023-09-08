5675ad10ff6a8741bce0e8d70ac4e250d2608178
Kwasi O-T <tweneboah21@gmail.com>
2023-09-08 06:48:55 +0000

    chgd joblist comp to class comp for loadmore btn

--- src/components/Joblist.js
+++ src/components/Joblist.js
@@ -1,17 +1,37 @@
-import React from 'react';
 import Data from '../data/data.json';
 import Jobs from './Jobs';
+import React, { Component } from 'react';
 
 
+class Joblist extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            itemsToShow: 12,
+            expanded: false
+        };
+    }
 
-function Joblist() {
-
-    const jobList = Data.map(job => <Jobs key = {job.id} job = {job} />);
-    return (
-        <div className='jobs'>
-            {jobList}
+    loadMore = () => {
+        this.setState({
+            itemsToShow: this.state.itemsToShow + 5,
+            expanded: true
+        });
+    };
+    
+    render() {
+        const jobList = Data.slice(0, this.state.itemsToShow).map(job =>
+            <Jobs key = {job.id} job = {job}/>);
+        return (
+        <div>
+            <div className='jobs'>
+                {jobList}
+                {!this.state.expanded && <button onClick={this.loadMore}>Load More</button>}
+            </div>
         </div>
-    );
+        )
+    }
 }
 
 export default Joblist;
