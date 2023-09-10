c334a8f271c6e2f0d3b5b4d3f09ac7dfb15f80f1
Kwasi O-T <tweneboah21@gmail.com>
2023-09-10 01:42:00 +0000

    added additional content to details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -11,10 +11,28 @@ function Details({job}) {
                     <p className='position'>{job.position}</p>
                     <p className='contract'>{job.contract}</p>
                 </div>
-                <div className='topDesc'>
+                <div className='topCompLabel'>
                     <p className='company'>{job.company}</p>
                     <p className='location'>{job.location}</p>
                     <p className='timePosted'>{job.postedAt}</p>
+                </div>
+                <div className='downCompLabel'>
+                    <p>{job.company}</p>
+                    <p>{job.location}</p>
+                    <p>{job.website}</p>
+                    <p>{job.description}</p>
+                    <h3>Position Overview:</h3>
+                    <p>{job.requirements.content}</p>
+                    <h3>Qualifications:</h3>
+                    <ul>
+                        {job.requirements.items.map(qualification => <li>{qualification}</li>).join('')}
+                    </ul>
+                    <h3>The Role</h3>
+                    <p>{job.role.content}</p>
+                    <h4>Key Responsibilities:</h4>
+                    <ul>
+                        {job.role.items.map(responsibilities => <li>{responsibilities}</li>).join('')}
+                    </ul>
                 </div>
             </div>
         </div>
